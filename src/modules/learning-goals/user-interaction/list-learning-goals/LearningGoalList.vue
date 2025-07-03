f9acb2f69c14f701cb98220ac6eda99456483703
koljapluemer <github@koljapluemer.com>
2025-07-03 15:58:04 +0200

    refactor learning goal list view

---
+++ src/modules/learning-goals/user-interaction/list-learning-goals/LearningGoalList.vue
@@ -0,0 +1,16 @@
+<template>
+  <table class="table table-zebra w-full">
+    <tbody>
+      <tr v-for="goal in goals" :key="goal.uid">
+        <td class="text-lg font-semibold">{{ goal.name }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+import type { LearningGoal } from '@/modules/learning-goals/types/LearningGoal'
+import { defineProps } from 'vue'
+
+defineProps<{ goals: LearningGoal[] }>()
+</script>
--- src/modules/learning-goals/user-interaction/list-learning-goals/ListLearningGoals.vue
+++ src/modules/learning-goals/user-interaction/list-learning-goals/ListLearningGoals.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="p-6 max-w-3xl mx-auto">
     <h1 class="text-2xl font-bold mb-4">Learning Goals</h1>
-    <table class="table table-zebra w-full">
-      <tbody>
-        <tr v-for="goal in learningGoals" :key="goal.uid">
-          <td class="text-lg font-semibold">{{ goal.name }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <LearningGoalList :goals="learningGoals" />
   </div>
 </template>
 
@@ -15,6 +9,7 @@
 import { ref, onMounted } from 'vue'
 import { db } from '@/modules/db/db-local/accessLocalDB'
 import type { LearningGoal } from '@/modules/learning-goals/types/LearningGoal'
+import LearningGoalList from './LearningGoalList.vue'
 
 const learningGoals = ref<LearningGoal[]>([])
 
